95a56cf5ba8f3a48e1c0fea0b1d702293c8bc242
Abduallah-Mohamed <Abduallahmohamed966@gmail.com>
2021-04-02 00:13:41 +0200

    Updates

---
+++ precache-manifest.22ced40406ea9da345c9e9df00eb25be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3536726707ec43a7a32a",
+    "url": "/Lights-Out/static/css/main.e321f73a.chunk.css"
+  },
+  {
+    "revision": "3536726707ec43a7a32a",
+    "url": "/Lights-Out/static/js/main.35367267.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/Lights-Out/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "bcedacc98465ac2cb9bb",
+    "url": "/Lights-Out/static/js/runtime~main.bcedacc9.js"
+  },
+  {
+    "revision": "7bacb0bcb6af1c8a937b88fcf34667b2",
+    "url": "/Lights-Out/index.html"
+  }
+];
